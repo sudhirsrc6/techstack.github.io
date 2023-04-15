44b3925c2c739375f23d4d5f9cbd9fbb4cf5c812
sudhirsrc6 <82930774+sudhirsrc6@users.noreply.github.com>
2023-04-15 22:10:29 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { PrimengModule } from 'src/primeng/primeng.module';
 
 @NgModule({
-  imports: [CommonModule, BrowserModule, ReactiveFormsModule],
+  imports: [CommonModule, BrowserModule, ReactiveFormsModule, PrimengModule],
   declarations: [],
   bootstrap: [AppComponent],
 })
---
+++ src/primeng/primeng.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  imports: [CommonModule, ButtonModule],
+  declarations: [],
+})
+export class PrimengModule {}
